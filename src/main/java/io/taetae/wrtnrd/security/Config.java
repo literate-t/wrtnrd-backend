bdb8184de48bff3d4a07959d177182b5db64f609
literate <gaekoon@gmail.com>
2024-03-20 22:51:24 +0900

    feat: api 경로와 cors를 설정한다

--- src/main/java/io/taetae/wrtnrd/security/Config.java
+++ src/main/java/io/taetae/wrtnrd/security/Config.java
@@ -1,31 +1,43 @@
 package io.taetae.wrtnrd.security;
 
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class Config {
 
-//  @Order(0)
-//  @Bean
-//  SecurityFilterChain apiConfig(HttpSecurity http) throws Exception {
-//
-//    http.securityMatcher("/api/**")
-//        .authorizeHttpRequests(registry -> {
-//          registry.requestMatchers("/api/signup", "/api/signin").permitAll()
-//              .anyRequest().authenticated();
-//        });
-//
-//    return http.build();
-//  }
+  private final UserDetailsService customUserDetailsService;
 
-//  @Order(1)
+  @Order(0)
+  @Bean
+  SecurityFilterChain apiConfig(HttpSecurity http) throws Exception {
+
+    http.securityMatcher("/api/**")
+        .authorizeHttpRequests(registry -> {
+          registry.requestMatchers("/api/csrfToken").permitAll()
+              .anyRequest().authenticated();
+        });
+
+    return http.build();
+  }
+
+  @Order(1)
   @Bean
   SecurityFilterChain commonConfig(HttpSecurity http) throws Exception {
 
@@ -36,7 +48,26 @@ public class Config {
 
     http.formLogin(Customizer.withDefaults());
 
+    http.userDetailsService(customUserDetailsService);
+
     return http.build();
   }
 
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+    corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    corsConfiguration.setAllowedHeaders(List.of("*"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+
+    return source;
+  }
 }
