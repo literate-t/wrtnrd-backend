cf4076b230af172f11796d9edc3a2c5144648785
literate <gaekoon@gmail.com>
2024-03-30 09:42:51 +0900

    feat: 회원가입 때 사용자 정보를 데이터베이스에 넣는 메서드를 추가한다

--- src/main/java/io/taetae/wrtnrd/service/UserService.java
+++ src/main/java/io/taetae/wrtnrd/service/UserService.java
@@ -1,17 +1,44 @@
 package io.taetae.wrtnrd.service;
 
+import io.taetae.wrtnrd.domain.dto.UserDto;
+import io.taetae.wrtnrd.domain.entity.Role;
+import io.taetae.wrtnrd.domain.entity.User;
+import io.taetae.wrtnrd.domain.entity.UserRole;
 import io.taetae.wrtnrd.domain.model.ProviderUser;
+import io.taetae.wrtnrd.repository.RoleRepository;
 import io.taetae.wrtnrd.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
   private final UserRepository userRepository;
+  private final RoleRepository repository;
+  private final PasswordEncoder passwordEncoder;
 
   public void register(String clientRegistrationId, ProviderUser providerUser) {
 
   }
+
+  @Transactional
+  public void register(UserDto userDto) {
+
+    Role roleUser = repository.findByName("ROLE_USER");
+    UserRole userRole = UserRole.create(roleUser);
+    User user = User.create(userDto, userRole);
+
+    changeToEncodedPassword(userDto, user);
+
+    userRepository.save(user);
+  }
+
+  private void changeToEncodedPassword(UserDto userDto, User user) {
+    String encoded = passwordEncoder.encode(userDto.password());
+    user.changePassword(encoded);
+  }
 }
+
