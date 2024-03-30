b6acdc2a3cb1b0b11fa20017aa6a4ff02098366a
literate <gaekoon@gmail.com>
2024-03-30 09:42:26 +0900

    feat: username으로 사용자를 찾는 메서드를 추가한다

--- src/main/java/io/taetae/wrtnrd/repository/UserRepository.java
+++ src/main/java/io/taetae/wrtnrd/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package io.taetae.wrtnrd.repository;
 
 import io.taetae.wrtnrd.domain.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  Optional<User> findByUsername(String username);
 }
