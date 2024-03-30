42beae29b0ac0f5477266eb192245b93d1ceb9f6
literate <gaekoon@gmail.com>
2024-03-30 09:25:09 +0900

    feat: 로그인을 할 때 백엔드에서 사용자를 직접 검증한다

--- src/main/java/io/taetae/wrtnrd/service/CustomUserDetailsService.java
+++ src/main/java/io/taetae/wrtnrd/service/CustomUserDetailsService.java
@@ -1,8 +1,43 @@
 package io.taetae.wrtnrd.service;
 
+import io.taetae.wrtnrd.domain.entity.User;
+import io.taetae.wrtnrd.domain.entity.UserRole;
+import io.taetae.wrtnrd.domain.model.PrincipalUser;
+import io.taetae.wrtnrd.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
-public class CustomUserDetailsService {
+public class CustomUserDetailsService extends AbstractUserService implements UserDetailsService {
 
+//  private final UserService userService;
+  private final UserRepository userRepository;
+
+
+  @Transactional
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username).orElse(new User());
+    if (null == user.getId()) {
+      throw new UsernameNotFoundException("User not found exception");
+    }
+
+    // TODO 비밀번호 확인
+
+    List<GrantedAuthority> roles = new ArrayList<>();
+    for (UserRole userRole : user.getUserRoles()) {
+      roles.add(new SimpleGrantedAuthority(userRole.getRole().getName()));
+    }
+
+    return new PrincipalUser(user.getUsername(), user.getPassword(), roles);
+  }
 }
