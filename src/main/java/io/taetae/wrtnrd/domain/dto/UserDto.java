0f6411d4965a4819b56188531ee23d698b600939
literate <gaekoon@gmail.com>
2024-04-05 14:40:46 +0900

    feat: User 정보를 dto에 담을 때 id를 제외한다

--- src/main/java/io/taetae/wrtnrd/domain/dto/UserDto.java
+++ src/main/java/io/taetae/wrtnrd/domain/dto/UserDto.java
@@ -2,5 +2,5 @@ package io.taetae.wrtnrd.domain.dto;
 
 import java.util.List;
 
-public record UserDto(Long id, String username, String password, String clientRegistrationId, String provider, List<String> roles) {
+public record UserDto(String username, String password, String clientRegistrationId, String provider, List<String> roles) {
 }
