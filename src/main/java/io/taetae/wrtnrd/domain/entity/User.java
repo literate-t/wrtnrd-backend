bdb0516b1206c6ef1e45c26572f63d205bb4897c
literate <gaekoon@gmail.com>
2024-04-21 15:05:23 +0900

    feat: UserDetails를 상속한다

--- src/main/java/io/taetae/wrtnrd/domain/entity/User.java
+++ src/main/java/io/taetae/wrtnrd/domain/entity/User.java
@@ -1,28 +1,31 @@
 package io.taetae.wrtnrd.domain.entity;
 
 import jakarta.annotation.Nullable;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Getter
 @ToString
-public class User {
+public class User implements UserDetails {
 
   @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Long id;
-  private String username;
-  private String password;
   private String email;
+  private String password;
   @Nullable
   private String clientRegistrationId;
   @Nullable
@@ -40,8 +43,7 @@ public class User {
       return false;
     }
     User user = (User) o;
-    return Objects.equals(getId(), user.getId()) && Objects.equals(getUsername(),
-        user.getUsername()) && Objects.equals(getPassword(), user.getPassword())
+    return Objects.equals(getId(), user.getId()) && Objects.equals(getPassword(), user.getPassword())
         && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(
         getClientRegistrationId(), user.getClientRegistrationId()) && Objects.equals(
         getProvider(), user.getProvider()) && Objects.equals(getUserRoles(),
@@ -50,8 +52,40 @@ public class User {
 
   @Override
   public int hashCode() {
-    return Objects.hash(getId(), getUsername(), getPassword(), getEmail(),
+    return Objects.hash(getId(), getPassword(), getEmail(),
         getClientRegistrationId(),
         getProvider(), getUserRoles());
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return getUserRoles().stream()
+        .map(userRole -> new SimpleGrantedAuthority(userRole.getRole().getName()))
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
   }
 }
