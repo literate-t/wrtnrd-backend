eb3cd7fda8f60b48a4a3e0a59405e3572e2a689c
literate <gaekoon@gmail.com>
2024-04-24 14:46:57 +0900

    feat: 토큰 엔티티를 추가한다
    - 액세스 토큰과 리프레시 토큰을 가진다
    - 각 토큰에 대한 만료, 철회 여부 필드를 가진다

---
+++ src/main/java/io/taetae/wrtnrd/domain/entity/Token.java
@@ -0,0 +1,38 @@
+package io.taetae.wrtnrd.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+  @Id @GeneratedValue
+  private Long id;
+
+  private String accessToken;
+
+  private String refreshToken;
+
+  private boolean accessExpired;
+
+  private boolean accessRevoked;
+
+  private boolean refreshExpired;
+
+  private boolean refreshRevoked;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+}
