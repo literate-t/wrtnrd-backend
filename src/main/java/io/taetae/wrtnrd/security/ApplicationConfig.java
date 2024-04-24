55c68fe69f645f048c18512d9eb1faca29c8a1db
literate <gaekoon@gmail.com>
2024-04-24 14:54:22 +0900

    feat: 커스텀으로 생성하는 빈을 모아둔다
    - 커스텀 프로바이더
    - 인증 과정에서 스프링의 기능을 사용하기 위해 AuthenticationManager를 빈으로 만든다

--- src/main/java/io/taetae/wrtnrd/security/ApplicationConfig.java
+++ src/main/java/io/taetae/wrtnrd/security/ApplicationConfig.java
@@ -4,7 +4,14 @@ import io.taetae.wrtnrd.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -12,15 +19,34 @@ public class ApplicationConfig {
 
   private final UserRepository userRepository;
 
-//  @Bean
-//  public AuthenticationProvider authenticationProvider() {
-//    DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-//  }
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+
+    DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    authenticationProvider.setUserDetailsService(userDetailsService());
+    authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+    return authenticationProvider;
+  }
+
+  // it is used for authentication
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+      throws Exception {
+
+    return configuration.getAuthenticationManager();
+  }
 
   @Bean
   public UserDetailsService userDetailsService() {
 
-    return username -> userRepository.findByEmail(username).orElse(null);
+    return username -> userRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
   }
 
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+
+    return new BCryptPasswordEncoder();
+  }
 }
