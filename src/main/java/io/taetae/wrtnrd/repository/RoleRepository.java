d6548dfa76c125712a18eced32b90c0cbf0e2f53
literate <gaekoon@gmail.com>
2024-04-30 11:37:48 +0900

    feat: 권한 이름으로 데이터를 조회할 수 있는 함수를 추가한다

--- src/main/java/io/taetae/wrtnrd/repository/RoleRepository.java
+++ src/main/java/io/taetae/wrtnrd/repository/RoleRepository.java
@@ -1,8 +1,10 @@
 package io.taetae.wrtnrd.repository;
 
 import io.taetae.wrtnrd.domain.entity.Role;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+  Optional<Role> findByRoleName(String roleName);
 }
