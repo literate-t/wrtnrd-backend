c88df15a1903362d0f2b788afe2d7b4fcfd4ae36
literate <gaekoon@gmail.com>
2024-04-30 11:43:19 +0900

    feat: Jwt 관련한 설정을 추가한다
    - 필터, 프로바이더를 추가한다
      - jwt 방식을 이용하면 세션 상태를 유지하지 않는 정책을 적용한다
    
    - 프론트엔드 url에 대해서만 cors를 허용한다

--- src/main/java/io/taetae/wrtnrd/security/SecurityConfig.java
+++ src/main/java/io/taetae/wrtnrd/security/SecurityConfig.java
@@ -1,41 +1,69 @@
 package io.taetae.wrtnrd.security;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import io.taetae.wrtnrd.jwt.JwtAuthenticationFilter;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-//  @Order(0)
-//  @Bean
-//  SecurityFilterChain apiConfig(HttpSecurity http) throws Exception {
-//
-//    http.securityMatcher("/api/**")
-//        .authorizeHttpRequests(registry -> {
-//          registry.requestMatchers("/api/signup", "/api/signin").permitAll()
-//              .anyRequest().authenticated();
-//        });
-//
-//    return http.build();
-//  }
+  private final AuthenticationProvider authenticationProvider;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-//  @Order(1)
   @Bean
-  SecurityFilterChain commonConfig(HttpSecurity http) throws Exception {
+  SecurityFilterChain apiConfig(HttpSecurity http) throws Exception {
 
-    http.authorizeHttpRequests(registry -> {
-      registry.requestMatchers("/signup", "/signin").permitAll()
-          .anyRequest().authenticated();
-    });
+    http.securityMatcher("/api/auth/**")
+        .authorizeHttpRequests(registry -> {
+          registry.requestMatchers("/api/auth/register", "/api/auth/authenticate", "/api/auth/refresh-token").permitAll()
+              .anyRequest().authenticated();
+        });
 
-    http.formLogin(Customizer.withDefaults());
+    http.sessionManagement(config -> config.sessionCreationPolicy(STATELESS))
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .cors((config -> config.configurationSource(corsConfigurationSource())))
+// TODO
+//        .logout(config -> config
+//            .addLogoutHandler(null)
+//            .logoutSuccessHandler(
+//                ((request, response, authentication) -> SecurityContextHolder.clearContext()))
+//            .logoutUrl("/api/auth/logout")
+//        )
+        ;
+
+    http.csrf(AbstractHttpConfigurer::disable);
 
     return http.build();
   }
 
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("http://localhost:3000"));
+    config.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
+    config.setAllowedHeaders(List.of("Content-Type"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return source;
+  }
 }
