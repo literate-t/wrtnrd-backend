b438b25ea7c97bf9594cf5dd2143ce493a58de52
literate <gaekoon@gmail.com>
2024-05-11 10:25:22 +0900

    feat: 새 토큰 발급 url을 상수로 추가한다 - 추후에 해당 url이 직접 들어간 곳을 상수로 대체한다

--- src/main/java/io/taetae/wrtnrd/util/Url.java
+++ src/main/java/io/taetae/wrtnrd/util/Url.java
@@ -3,4 +3,6 @@ package io.taetae.wrtnrd.util;
 public class Url {
 
   public static final String REGISTER_URL = "/api/auth/register";
+
+  public static final String NEW_TOKENS_URL = "/api/auth/new-tokens";
 }
