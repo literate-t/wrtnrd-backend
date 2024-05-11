86f608804dec3beafd43363e5fc21a94fa2f9030
literate <gaekoon@gmail.com>
2024-05-11 10:20:15 +0900

    feat: 리프레시 토큰 검증 로직을 추가한다
    - 리프레시 토큰이 만료된 거면 401을 응답해서 프론트엔드에서 로그인 창으로 갈 수 있도록 한다
    - 이 때 쿠키를 삭제하기 위한 로직을 추가한다(deleteTokenCookies)

--- src/main/java/io/taetae/wrtnrd/jwt/JwtAuthenticationFilter.java
+++ src/main/java/io/taetae/wrtnrd/jwt/JwtAuthenticationFilter.java
@@ -1,13 +1,15 @@
 package io.taetae.wrtnrd.jwt;
 
 import static io.taetae.wrtnrd.util.Constant.ACCESS_TOKEN;
+import static io.taetae.wrtnrd.util.Constant.REFRESH_TOKEN;
+import static io.taetae.wrtnrd.util.Url.NEW_TOKENS_URL;
 import static io.taetae.wrtnrd.util.Url.REGISTER_URL;
 import static io.taetae.wrtnrd.util.Util.checkSameString;
 
-import io.taetae.wrtnrd.repository.TokenRepository;
 import io.taetae.wrtnrd.util.Util;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -16,24 +18,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+// TODO Token에 ac, rf 등으로 타입을 두고 필드를 token 하나만 두면 중복 로직을 통합할 것
 
 @RequiredArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtService jwtService;
-  private final UserDetailsService customUserDetailsService;
-  private final TokenRepository tokenRepository;
 
   @Override
   protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
       @NonNull FilterChain filterChain) throws ServletException, IOException {
 
     String accessToken = Util.checkCookieAndGetValue(request, ACCESS_TOKEN);
+    String refreshToken = Util.checkCookieAndGetValue(request, REFRESH_TOKEN);
     String targetUrl = getRequestURI(request);
 
     boolean isRegisterUrl = checkSameString(targetUrl, REGISTER_URL);
@@ -44,25 +46,77 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       return;
     }
 
+    String userEmail = null;
+    try {
+      userEmail = jwtService.extractUsername(accessToken);
+    } catch (RuntimeException e) {
 
-    String userEmail = jwtService.extractUsername(accessToken);
-    // if an authentication already exists, this is unnecessary
+      if (checkSameString(targetUrl, NEW_TOKENS_URL)) {
+        try {
+          userEmail = jwtService.extractUsername(refreshToken);
+
+          if (null != userEmail && null == SecurityContextHolder.getContext().getAuthentication()) {
+            authenticateWithRefreshToken(request, userEmail, refreshToken);
+          }
+
+          filterChain.doFilter(request, response);
+
+          return;
+        } catch(RuntimeException nestedException) {
+          deleteTokenCookies(request, response);
+          response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+
+          return;
+        }
+      }
+
+      response.sendError(HttpServletResponse.SC_FORBIDDEN);
+
+      return;
+    }
+
     if (null != userEmail && null == SecurityContextHolder.getContext().getAuthentication()) {
-      authenticate(request, userEmail, accessToken);
+      authenticateWithAccessToken(request, userEmail, accessToken);
     }
 
     filterChain.doFilter(request, response);
+  }
+
+  private void deleteTokenCookies(HttpServletRequest request, HttpServletResponse response) {
+
+    Cookie accessTokenCookie = Util.getCookie(request, ACCESS_TOKEN).orElse(null);
+    Cookie refreshTokenCookie = Util.getCookie(request, REFRESH_TOKEN).orElse(null);
+
+    if (null != accessTokenCookie) {
+      accessTokenCookie.setMaxAge(0);
+      accessTokenCookie.setPath("/");
+      response.addCookie(accessTokenCookie);
+    }
+    if (null != refreshTokenCookie) {
+      refreshTokenCookie.setMaxAge(0);
+      refreshTokenCookie.setPath("/");
+      response.addCookie(refreshTokenCookie);
+    }
   }
 
   private String getRequestURI(HttpServletRequest request) {
     return request.getRequestURI();
   }
 
-  private void authenticate(HttpServletRequest request, String userEmail, String accessToken) {
+  private void authenticateWithAccessToken(HttpServletRequest request, String userEmail, String accessToken) {
 
-    UserDetails user = loadUserDetails(userEmail);
+    UserDetails user = jwtService.loadUserDetails(userEmail);
 
-    if (isTokenAuthenticationValid(accessToken, user)) {
+    if (jwtService.isAccessTokenValid(accessToken, user)) {
+      setAuthenticationContext(request, user);
+    }
+  }
+
+  private void authenticateWithRefreshToken(HttpServletRequest request, String userEmail, String refreshToken) {
+
+    UserDetails user = jwtService.loadUserDetails(userEmail);
+
+    if (jwtService.isRefreshTokenValid(refreshToken, user)) {
       setAuthenticationContext(request, user);
     }
   }
@@ -76,17 +130,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-  }
-
-  private UserDetails loadUserDetails(String userEmail) {
-    return customUserDetailsService.loadUserByUsername(userEmail);
-  }
-
-  private boolean isTokenAuthenticationValid(String accessToken, UserDetails user) {
-
-    boolean isStoredAccessValid = tokenRepository.findByAccessToken(accessToken)
-        .map(token -> !token.isAccessExpired() && !token.isAccessRevoked()).orElse(false);
-
-    return isStoredAccessValid && jwtService.isTokenValid(accessToken, user);
   }
 }
