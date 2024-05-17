cf2fffc9490e35eb0fbced9a93b3cbbf26c2b927
literate <gaekoon@gmail.com>
2024-05-17 11:58:30 +0900

    feat: Post 엔티티의 필드 정보를 일부 변경한다
    - 본문 필드를 content가 아니라 body로, 컬럼 타입을 varchar(256)가 아닌 TEXT로 변경한다
    - 시간 필드를 LocalDateTime에서 String으로 변경한다
      - 프론트엔드에서 String으로 포매팅해서 보낸다

--- src/main/java/io/taetae/wrtnrd/domain/entity/Post.java
+++ src/main/java/io/taetae/wrtnrd/domain/entity/Post.java
@@ -4,17 +4,23 @@ import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.SEQUENCE;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Getter
 public class Post {
@@ -24,10 +30,12 @@ public class Post {
   private Long id;
 
   private String title;
-  private String content;
+  private String author;
+  @Column(columnDefinition = "TEXT")
+  private String body;
   private String imagePath;
-  private LocalDateTime createdAt;
-  private LocalDateTime lastModifiedAt;
+  private String createdAt;
+  private String lastModifiedAt;
 
   @ManyToOne(fetch = LAZY)
   @JoinColumn(name = "user_id")
