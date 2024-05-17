9304a0d9e16fe31c6bb6933d0467f6a4d544cc54
literate <gaekoon@gmail.com>
2024-05-17 11:52:34 +0900

    feat: 컨트롤러에 유저 정보를 전달할 때를 위해 User 객체를 추가한다

--- src/main/java/io/taetae/wrtnrd/domain/dto/AuthenticationResponseDto.java
+++ src/main/java/io/taetae/wrtnrd/domain/dto/AuthenticationResponseDto.java
@@ -1,5 +1,11 @@
 package io.taetae.wrtnrd.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.taetae.wrtnrd.domain.entity.User;
 
-public record AuthenticationResponseDto(@JsonProperty("ac")String accessToken, @JsonProperty("rf")String refreshToken) {}
+public record AuthenticationResponseDto(User user, @JsonProperty("ac")String accessToken, @JsonProperty("rf")String refreshToken) {
+
+  public AuthenticationResponseDto(String accessToken, String refreshToken) {
+    this(null, accessToken, refreshToken);
+  }
+}
