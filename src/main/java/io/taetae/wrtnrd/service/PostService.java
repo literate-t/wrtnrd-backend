9d3f14f9ad60e943931421cc7afbca4046d38995
literate <gaekoon@gmail.com>
2024-05-17 12:01:23 +0900

    add: PostService를 추가한다
    - 새로운 포스트를 저장하는 save 함수를 정의한다

---
+++ src/main/java/io/taetae/wrtnrd/service/PostService.java
@@ -0,0 +1,37 @@
+package io.taetae.wrtnrd.service;
+
+import io.taetae.wrtnrd.domain.dto.PostRequestDto;
+import io.taetae.wrtnrd.domain.entity.Post;
+import io.taetae.wrtnrd.domain.entity.User;
+import io.taetae.wrtnrd.repository.PostRepository;
+import io.taetae.wrtnrd.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+  private final UserRepository userRepository;
+  private final PostRepository postRepository;
+
+  public Post save(PostRequestDto post) {
+
+    Long userId = post.userId();
+    User user = userRepository.findById(userId).orElse(null);
+
+    if (null != user) {
+      // TODO User 엔티티에 nickname 추가하고 회원가입 때도 추가하고, author에 넣을 것
+      Post newPost = Post.builder().user(user)
+          .author(null)
+          .title(post.title())
+          .body(post.body())
+          .createdAt(post.createdAt())
+          .build();
+
+      return postRepository.save(newPost);
+    }
+
+    return null;
+  }
+}
