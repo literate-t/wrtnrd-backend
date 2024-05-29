ecac9aac55ae7b0cabf38d2a86802534dd1f3a27
literate <gaekoon@gmail.com>
2024-05-29 19:18:33 +0900

    feat: 마이페이지에서의 요청을 처리한다
    - 유저 정보를 바꾸기 때문에 UserController 추가
    - UserRepository author 조회하는 함수를 추가
    - password, author 확인하고 변경하는 비즈니스 로직이 있는 UserService 추가

---
+++ src/main/java/io/taetae/wrtnrd/controller/UserController.java
@@ -0,0 +1,120 @@
+package io.taetae.wrtnrd.controller;
+
+import static io.taetae.wrtnrd.util.Constant.ACCESS_TOKEN;
+import static io.taetae.wrtnrd.util.Constant.ACCESS_TOKEN_MISSING;
+import static io.taetae.wrtnrd.util.Constant.AUTHOR_CHECK_FAILURE;
+import static io.taetae.wrtnrd.util.Constant.AUTHOR_NOT_UPDATED;
+import static io.taetae.wrtnrd.util.Constant.AUTHOR_UPDATED;
+import static io.taetae.wrtnrd.util.Constant.AVAILABLE_AUTHOR;
+import static io.taetae.wrtnrd.util.Constant.INTERNAL_SERVER_ERROR;
+import static io.taetae.wrtnrd.util.Constant.NO_PARAM_FROM_REQUEST;
+import static io.taetae.wrtnrd.util.Constant.PASSWORD_CHANGED_SUCCESSFULLY;
+import static io.taetae.wrtnrd.util.Constant.PASSWORD_CHECKED_SUCCESSFULLY;
+import static io.taetae.wrtnrd.util.Constant.PASSWORD_CHECK_FAILURE;
+import static io.taetae.wrtnrd.util.Constant.USER_NOT_FOUND_FROM_ACCESS_TOKEN;
+
+import io.taetae.wrtnrd.domain.dto.AuthorRequestDto;
+import io.taetae.wrtnrd.domain.dto.PasswordChangeDto;
+import io.taetae.wrtnrd.domain.dto.PasswordCheckDto;
+import io.taetae.wrtnrd.jwt.JwtService;
+import io.taetae.wrtnrd.service.UserService;
+import io.taetae.wrtnrd.util.Util;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+  private final JwtService jwtService;
+  private final UserService userService;
+
+  @PostMapping("/check-password")
+  public ResponseEntity<String> checkPassword(@RequestBody PasswordCheckDto passwordDto, HttpServletRequest request) {
+
+    Cookie accessCookie = Util.getCookie(request, ACCESS_TOKEN).orElse(null);
+
+    if (null == accessCookie) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ACCESS_TOKEN_MISSING);
+    }
+
+    try {
+      String extractUsername = jwtService.extractUsername(accessCookie.getValue());
+      boolean result = userService.checkPassword(extractUsername, passwordDto.password());
+
+      if (result) {
+        return ResponseEntity.ok(PASSWORD_CHECKED_SUCCESSFULLY);
+      } else {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(PASSWORD_CHECK_FAILURE);
+      }
+    } catch (RuntimeException e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("Internal server error: " + e.getMessage());
+    }
+  }
+
+  @PostMapping("/change-password")
+  public ResponseEntity<String> changePassword(@RequestBody PasswordChangeDto passwordChangeDto, HttpServletRequest request) {
+
+    Cookie accessCookie = Util.getCookie(request, ACCESS_TOKEN).orElse(null);
+
+    if (null == accessCookie) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+          .body(ACCESS_TOKEN_MISSING);
+    }
+
+    try {
+      String extractUsername = jwtService.extractUsername(accessCookie.getValue());
+      boolean result = userService.changeUserPassword(extractUsername,
+          passwordChangeDto.newPassword());
+
+      if (result) {
+        return ResponseEntity.ok(PASSWORD_CHANGED_SUCCESSFULLY);
+      } else {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(USER_NOT_FOUND_FROM_ACCESS_TOKEN);
+      }
+    } catch (RuntimeException e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body(INTERNAL_SERVER_ERROR +": " + e.getMessage());
+    }
+  }
+
+  @PostMapping("/check-author-duplicated")
+  public ResponseEntity<String> checkAuthorDuplicated(@RequestBody AuthorRequestDto authorRequestDto) {
+
+    if (null == authorRequestDto.author() || authorRequestDto.author().isEmpty()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(NO_PARAM_FROM_REQUEST);
+    }
+
+    boolean result = userService.isAuthorAvailable(authorRequestDto.author());
+    if (result) {
+      return ResponseEntity.ok(AVAILABLE_AUTHOR);
+    } else {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(AUTHOR_CHECK_FAILURE);
+    }
+  }
+
+  @PostMapping("/change-author")
+  public ResponseEntity<String> changeAuthor(@RequestBody AuthorRequestDto authorRequestDto) {
+
+    if (null == authorRequestDto.author() || authorRequestDto.author().isEmpty()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(NO_PARAM_FROM_REQUEST);
+    }
+
+    boolean result = userService.changeAuthor(authorRequestDto.userId(), authorRequestDto.author());
+    if (result) {
+      return ResponseEntity.ok(AUTHOR_UPDATED);
+    } else {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(AUTHOR_NOT_UPDATED);
+    }
+  }
+}
--- src/main/java/io/taetae/wrtnrd/service/UserService.java
+++ src/main/java/io/taetae/wrtnrd/service/UserService.java
@@ -1,17 +1,69 @@
 package io.taetae.wrtnrd.service;
 
-import io.taetae.wrtnrd.domain.model.ProviderUser;
 import io.taetae.wrtnrd.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
   private final UserRepository userRepository;
+  private final AuthenticationManager authenticationManager;
+  private final PasswordEncoder passwordEncoder;
 
-  public void register(String clientRegistrationId, ProviderUser providerUser) {
+  @Transactional
+  public boolean checkPassword(String username, String password) {
+    try {
+      authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(username, password));
+      return true;
+    } catch (AuthenticationException e) {
+      return false;
+    }
+  }
 
+  @Transactional
+  public boolean changeUserPassword(String userEmail, String newPassword) {
+
+    return userRepository.findByEmail(userEmail).map(user -> {
+      user.setPassword(passwordEncoder.encode(newPassword));
+      log.info("Password changed for user: {}", userEmail);
+      return true;
+    }).orElseGet(() -> {
+      log.warn("No user found for email: {}", userEmail);
+      return false;
+    });
+  }
+
+  public boolean isAuthorAvailable(String author) {
+
+    return userRepository.findByAuthor(author).map((user) -> {
+      log.warn("author exists: {}", user.getAuthor());
+      return false;
+    }).orElseGet(() -> {
+      log.warn("author doesn't exist: {}", author);
+      return true;
+    });
+  }
+
+  @Transactional
+  public boolean changeAuthor(Long id, String author) {
+
+    return userRepository.findById(id).map(user -> {
+      user.setAuthor(author);
+      log.info("Author changed for user: {}", author);
+      return true;
+    }).orElseGet(()-> {
+      log.warn("Author update failure: {}", author);
+      return false;
+    });
   }
 }
