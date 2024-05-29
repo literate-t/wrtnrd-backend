64a0d24e5be46e3705edbeec3baf3e9aff8e6bd6
literate <gaekoon@gmail.com>
2024-05-29 19:14:41 +0900

    feat: 마이페이지에 필요한 문자열을 상수로 모아둔다
    - 추후 유지 보수 때 해당 클래스만 들여다보면 된다

--- src/main/java/io/taetae/wrtnrd/util/Constant.java
+++ src/main/java/io/taetae/wrtnrd/util/Constant.java
@@ -2,6 +2,24 @@ package io.taetae.wrtnrd.util;
 
 public class Constant {
 
+  // common
+  public static final String INTERNAL_SERVER_ERROR = "Internal server error";
+  public static final String NO_PARAM_FROM_REQUEST = "Required data is empty";
+
+  // token
   public static final String ACCESS_TOKEN = "ac";
   public static final String REFRESH_TOKEN = "rf";
+
+  // mypage-password
+  public static final String PASSWORD_CHANGED_SUCCESSFULLY = "Password changed successfully";
+  public static final String PASSWORD_CHECKED_SUCCESSFULLY = "Password checked successfully";
+  public static final String PASSWORD_CHECK_FAILURE= "Password check failure";
+  public static final String ACCESS_TOKEN_MISSING = "Access token is missing";
+  public static final String USER_NOT_FOUND_FROM_ACCESS_TOKEN = "User not found from access token";
+
+  // mypage-author
+  public static final String AVAILABLE_AUTHOR = "Author is available";
+  public static final String AUTHOR_CHECK_FAILURE = "Author check failure";
+  public static final String AUTHOR_UPDATED = "Author is updated";
+  public static final String AUTHOR_NOT_UPDATED = "Author is not updated";
 }
