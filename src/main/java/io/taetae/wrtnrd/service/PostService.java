5772d46efc22e34d25659d57a82dd100e1126dd5
literate <gaekoon@gmail.com>
2024-06-01 17:08:40 +0900

    feat: 포스트 좋아요 요청에 대한 비즈니스 로직을 추가한다
    - 좋아요 토글
    - 현재 페이지에 있는 포스트 중 좋아요 포스트를 식별하기

--- src/main/java/io/taetae/wrtnrd/service/PostService.java
+++ src/main/java/io/taetae/wrtnrd/service/PostService.java
@@ -1,19 +1,31 @@
 package io.taetae.wrtnrd.service;
 
+import static io.taetae.wrtnrd.util.Constant.PAGE_SIZE;
+
 import io.taetae.wrtnrd.domain.dto.PostRequestDto;
+import io.taetae.wrtnrd.domain.dto.PostResponseDto;
 import io.taetae.wrtnrd.domain.entity.Post;
+import io.taetae.wrtnrd.domain.entity.PostLike;
 import io.taetae.wrtnrd.domain.entity.User;
+import io.taetae.wrtnrd.repository.PostLikeRepository;
 import io.taetae.wrtnrd.repository.PostRepository;
 import io.taetae.wrtnrd.repository.UserRepository;
+import java.util.List;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PostService {
 
   private final UserRepository userRepository;
   private final PostRepository postRepository;
+  private final PostLikeRepository postLikeRepository;
 
   public Post save(PostRequestDto post) {
 
@@ -32,5 +44,61 @@ public class PostService {
     }
 
     return null;
+  }
+
+  public boolean toggleLike(Long userId, Long postId) {
+
+    User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid user ID"));
+    Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("Invalid post ID"));
+
+    return postLikeRepository.findByUserIdAndPostId(userId, postId).map(postLike -> {
+      postLikeRepository.delete(postLike);
+      return false;
+    })
+    .orElseGet(() -> {
+      PostLike postLike = PostLike.create(user, post);
+      postLikeRepository.save(postLike);
+      return true;
+    });
+  }
+
+  public List<PostResponseDto> getPosts() {
+    return postRepository.findAllByOrderByCreatedAtDesc(
+            PageRequest.of(0, PAGE_SIZE))
+        .map(
+            post -> new PostResponseDto(
+                post.getId(),
+                post.getTitle(),
+                post.getUser().getAuthor(),
+                post.getUser().getDescription(),
+                post.getBody(),
+                post.getCreatedAt(),
+                false)
+        ).toList();
+  }
+
+  public List<PostResponseDto> getPostsWithLikes(int pageNumber, Long userId) {
+
+    // 한 페이지의 포스트 리스트 구하기
+    Page<Post> posts = postRepository.findAllByOrderByCreatedAtDesc(
+        PageRequest.of(pageNumber, PAGE_SIZE));
+
+    // 한 페이지의 포스트 id 리스트 구하기
+    List<Long> postIds = posts.getContent().stream().map(Post::getId).toList();
+
+    // PostLikeRepository에서 해당 포스트에 좋아요한 포스트 구하기
+    Set<Long> postLikeIds = postLikeRepository.findPostIdByUserIdAndPostIdIn(userId, postIds);
+
+    // 포스트 내려줄 때 해당 포스트에 좋아요 체크하기
+    return posts.map(post -> new PostResponseDto(
+        post.getId(),
+        post.getTitle(),
+        post.getUser().getAuthor(),
+        post.getUser().getDescription(),
+        post.getBody(),
+        post.getCreatedAt(),
+        postLikeIds.contains((post.getId())
+    ))
+    ).toList();
   }
 }
