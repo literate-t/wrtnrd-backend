0dd984d7dad00e78ff08b4ee6f2ee99a69a6419f
literate <gaekoon@gmail.com>
2024-06-01 17:04:08 +0900

    feat: 포스트 좋아요에 대한 응답 메서드를 추가한다
    - 새로운 포스트를 작성하면 좋아요는 false를 설정한다
    - 좋아요 요청을 받으면 토글 형태로 처리한다
      - 좋아요를 하면 true, 좋아요 취소는 false

--- src/main/java/io/taetae/wrtnrd/controller/PostController.java
+++ src/main/java/io/taetae/wrtnrd/controller/PostController.java
@@ -1,36 +1,39 @@
 package io.taetae.wrtnrd.controller;
 
+import io.taetae.wrtnrd.domain.dto.PostLikeRequestDto;
 import io.taetae.wrtnrd.domain.dto.PostRequestDto;
 import io.taetae.wrtnrd.domain.dto.PostResponseDto;
+import io.taetae.wrtnrd.domain.dto.UserRequestDto;
 import io.taetae.wrtnrd.domain.entity.Post;
-import io.taetae.wrtnrd.repository.PostRepository;
 import io.taetae.wrtnrd.service.PostService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/post")
 @RestController
 public class PostController {
 
   private final PostService postService;
-  private final PostRepository postRepository;
 
-  @GetMapping("/list")
-  public ResponseEntity<List<PostResponseDto>> getAllPosts() {
+  @PostMapping("/list")
+  public ResponseEntity<List<PostResponseDto>> getAllPosts(@RequestBody UserRequestDto user) {
 
-    List<PostResponseDto> list = postRepository.findAllByOrderByCreatedAtDesc(PageRequest.of(0, 10))
-        .map(
-            post -> new PostResponseDto(post.getId(), post.getTitle(), post.getUser().getAuthor(), post.getUser().getDescription(), post.getBody(),
-                post.getCreatedAt())).toList();
+    if (null == user.userId()) {
+      List<PostResponseDto> list = postService.getPosts();
+      return ResponseEntity.ok(list);
+    }
+
+    List<PostResponseDto> list = postService.getPostsWithLikes(0, user.userId());
 
     return ResponseEntity.ok(list);
   }
@@ -41,6 +44,33 @@ public class PostController {
 
     Post post = postService.save(postRequestDto);
 
-    return ResponseEntity.ok(new PostResponseDto(post.getId(), post.getTitle(), post.getUser().getAuthor(), post.getUser().getDescription(), post.getBody(), post.getCreatedAt()));
+    return ResponseEntity.ok(
+        new PostResponseDto(
+            post.getId(),
+            post.getTitle(),
+            post.getUser().getAuthor(),
+            post.getUser().getDescription(),
+            post.getBody(),
+            post.getCreatedAt(),
+            false
+        )
+    );
+  }
+
+  @Transactional
+  @PostMapping("/like")
+  public ResponseEntity<Boolean> postLike(@RequestBody PostLikeRequestDto postLikeDto) {
+
+    try {
+      boolean result = postService.toggleLike(postLikeDto.userId(), postLikeDto.postId());
+      if (result) {
+        return ResponseEntity.ok(true);
+      } else {
+        return ResponseEntity.ok(false);
+      }
+    } catch (RuntimeException e) {
+      log.error(e.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
   }
 }
