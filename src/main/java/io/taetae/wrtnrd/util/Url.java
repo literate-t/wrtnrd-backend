19be62ec180c604975b2cbe673b1675d960a8445
literate <gaekoon@gmail.com>
2024-06-21 21:02:11 +0900

    feat: logout url을 추가한다

--- src/main/java/io/taetae/wrtnrd/util/Url.java
+++ src/main/java/io/taetae/wrtnrd/util/Url.java
@@ -5,4 +5,6 @@ public class Url {
   public static final String REGISTER_URL = "/api/auth/register";
 
   public static final String NEW_TOKENS_URL = "/api/auth/new-tokens";
+
+  public static final String LOGOUT_URL = "/api/auth/logout";
 }
