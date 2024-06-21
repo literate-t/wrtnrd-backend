a91fdbcfc458ec0e2697f9df8abe50e00f758d69
literate <gaekoon@gmail.com>
2024-06-21 20:56:13 +0900

    feat: Config를 변경한다
    - /api/auth/logout을 permitAll을 한다
      - jwt 방식에서는 리프레시 토큰이 이미 만료된 상태에서 서버에 요청할 수 있어야 한다
    
    - cors 설정에 authorization 헤더 넣어준다
      - authorization 헤더는 기본 허용 헤더가 아니다

--- src/main/java/io/taetae/wrtnrd/security/SecurityConfig.java
+++ src/main/java/io/taetae/wrtnrd/security/SecurityConfig.java
@@ -1,9 +1,15 @@
 package io.taetae.wrtnrd.security;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-import io.taetae.wrtnrd.jwt.JwtAuthenticationFilter;
-import jakarta.servlet.http.HttpServletResponse;
+import io.taetae.wrtnrd.filter.jwt.JwtAuthenticationFilter;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,7 +19,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -37,17 +42,14 @@ public class SecurityConfig {
     http.securityMatcher("/api/**")
         .authorizeHttpRequests(registry -> {
           registry.requestMatchers("/api/auth/register", "/api/auth/authenticate",
-                  "/api/post/list").permitAll() // "/api/auth/new-token"
+                  "/api/post/list", "/api/auth/logout").permitAll() // "/api/auth/new-token"
               .anyRequest().authenticated();
         });
 
     http.sessionManagement(config -> config.sessionCreationPolicy(STATELESS))
         .authenticationProvider(authenticationProvider)
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-        .cors((config -> config.configurationSource(corsConfigurationSource())))
-        .exceptionHandling(config -> config.authenticationEntryPoint(((request, response, authException) -> {
-          response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied: Authentication is required to access this resource.");
-        })));
+        .cors((config -> config.configurationSource(corsConfigurationSource())));
 
     http.csrf(AbstractHttpConfigurer::disable);
 
@@ -59,8 +61,8 @@ public class SecurityConfig {
 
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowedOrigins(List.of(frontendUrl));
-    config.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
-    config.setAllowedHeaders(List.of("Content-Type"));
+    config.setAllowedMethods(List.of(GET.name(), OPTIONS.name(), POST.name(), PUT.name(), DELETE.name()));
+    config.setAllowedHeaders(List.of(AUTHORIZATION, CONTENT_TYPE));
     config.setAllowCredentials(true);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
